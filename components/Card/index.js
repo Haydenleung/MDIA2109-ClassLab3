b9ab12a4022e9ab433afb96081d2858b8dd1ee09
cleung222@my.bcit.ca <cleung222@my.bcit.ca>
2023-01-22 22:51:18 -0800

    initial update

--- components/Card/index.js
+++ components/Card/index.js
@@ -8,11 +8,11 @@ export default function Card({
     return(
         <div style={{fontSize:font}}>
             {
-                studentNumber === "A0004567" ?  <img src={'icon/person-01.svg'} /> :
-                studentNumber === "A0001234" ?  <img src={'icon/person-02.svg'} /> :
-                studentNumber === "A0007890" ?  <img src={'icon/person-03.svg'} /> :
-                studentNumber === "A0007828" ?  <img src={'icon/person-04.svg'} /> :
-                studentNumber === "A0003425" ?  <img src={'icon/person-05.svg'} /> :
+                studentNumber === "A0004567" ?  <img src={'icon/person01.svg'} /> :
+                studentNumber === "A0001234" ?  <img src={'icon/person02.svg'} /> :
+                studentNumber === "A0007890" ?  <img src={'icon/person03.svg'} /> :
+                studentNumber === "A0007828" ?  <img src={'icon/person04.svg'} /> :
+                studentNumber === "A0003425" ?  <img src={'icon/person05.svg'} /> :
                                                 <img src={image}/>
             }
             <div>{degree}</div>
